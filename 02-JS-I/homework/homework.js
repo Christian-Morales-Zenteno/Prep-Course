f7eaba0bebc309480450676da55385f4ea180afd
unknown <christian.morales.zenteno@gmail.com>
2022-08-12 13:08:54 -0300

    Second attempt JS-1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,7 +73,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.lengh == str2.lengh) {
+  if (str1.length == str2.length) {
     return true;
   } else return false;
 }
@@ -125,37 +125,37 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return math.pow(num,2);
+  return Math.pow(num, 2);
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return math.pow(num,3);
+  return Math.pow(num, 3);
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  return math.pow(num,exponent);
+  return Math.pow(num, exponent);
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  return math.round(num);
+  return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return math.ceil(num);
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return math.random();
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -234,9 +234,11 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra.length > 1) {
     return "Dato incorrecto"
-   } else if (letra.length == 1 && letra == a || e || i || o || u) {
+   } else if (letra !== "a" || "e" || "i" || "o" || "u") {
+    return "Dato incorrecto"
+   } else if (letra === "a" || "e" || "i" || "o" || "u") {
     return "Es vocal"
-   } else return "Dato incorrecto"
+   }
 }
 
 // No modificar nada debajo de esta línea
