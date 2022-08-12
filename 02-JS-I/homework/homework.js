435d1ad1970f00da19b2074c55c9865ce529881c
unknown <christian.morales.zenteno@gmail.com>
2022-08-12 13:42:49 -0300

    Homework final version

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -233,12 +233,10 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length > 1) {
-    return "Dato incorrecto"
-   } else if (letra !== "a" || "e" || "i" || "o" || "u") {
-    return "Dato incorrecto"
-   } else if (letra === "a" || "e" || "i" || "o" || "u") {
-    return "Es vocal"
-   }
+    return "Dato incorrecto";
+   } else if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+    return "Es vocal";
+   } else return "Dato incorrecto"
 }
 
 // No modificar nada debajo de esta línea
